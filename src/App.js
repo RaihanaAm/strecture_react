30a0f72682f9e3429a939b1173951221e9890254
RaihanaAm <raihana.amrani999@gmail.com>
2023-07-16 15:31:02 +0100

    besmillah

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenNib } from '@fortawesome/free-solid-svg-icons'
+
+
+export const App = () => {
+
+  return (
+    <div>
+      <FontAwesomeIcon className='text-primary' icon={faPenNib} />
+    </div>
+  )
+
+}
